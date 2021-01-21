ad0a85e028f2fa5445c5528c1c06f30767853cca
amine <miino@live.fr>
2021-01-21 23:39:39 +0100

    firenotification on extension load

--- background.js
+++ background.js
@@ -1,5 +1,15 @@
 //try to fire a notification every 1 minute
-
+chrome.notifications.create(
+    "1", {
+    iconUrl: chrome.runtime.getURL("icons/128.png"),
+    title: "This should be a notification",
+    type: "basic",
+    message: "Notification body",
+    isClickable: true,
+    priority: 2,
+},
+    function () { }
+);
 
 setInterval(function () {
 
